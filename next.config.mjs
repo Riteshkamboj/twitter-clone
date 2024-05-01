1fd5f90c580e669f54e28c9f60a592335773e3e1
RiteshKamboj <riteshkumarabc23@gmail.com>
2024-05-01 14:00:14 +0530

    Added basic homepage layout

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["th.bing.com","cdn-icons-png.flaticon.com"],
+  },
+};
+
+export default nextConfig;
